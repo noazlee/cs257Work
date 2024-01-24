b38478d2ce906521f416af8cf99e838c9a32991f
Noah Lee <156212285+noazlee@users.noreply.github.com>
2024-01-24 09:40:42 -0600

    Update createtables.py
    modified data types

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -13,9 +13,9 @@ def create_tables():
     command2 = """CREATE TABLE cities (
                 city_name VARCHAR(255) NOT NULL,
                 state_name VARCHAR(255) NOT NULL,
-                city_pop int(255) NOT NULL,
-                city_lat real(255) NOT NULL,
-                city_lon real(255) NOT NULL
+                city_pop NUMERIC NOT NULL,
+                city_lat float(255) NOT NULL,
+                city_lon float(255) NOT NULL
                 )"""
 
     conn = None
