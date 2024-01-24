7a5d188a2c64d5bf53e2bfc0fdbce0b21f3a1c0a
Noah Lee <156212285+noazlee@users.noreply.github.com>
2024-01-24 09:27:54 -0600

    Update createtables.py
    Changed " to """

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -5,18 +5,18 @@ from config import config
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
-    command1 = "CREATE TABLE states (
+    command1 = """CREATE TABLE states ( 
             state_name VARCHAR(255) NOT NULL,
             state_id SERIAL PRIMARY KEY 
-            ) "
+            ) """
         
-    command2 = "CREATE TABLE cities (
+    command2 = """CREATE TABLE cities (
                 city_name VARCHAR(255) NOT NULL,
                 state_name VARCHAR(255) NOT NULL,
                 city_pop int(255) NOT NULL,
                 city_lat real(255) NOT NULL,
                 city_lon real(255) NOT NULL
-                )"
+                )"""
 
     conn = None
     try:
