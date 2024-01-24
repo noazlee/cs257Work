3cc4dd0b2ade53c830fbe60880f7faeef4358bcb
Noah Lee <156212285+noazlee@users.noreply.github.com>
2024-01-24 09:21:32 -0600

    Update createtables.py
    Fixed bug in python code

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -5,22 +5,19 @@ from config import config
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
-    commands = (
-        
-        CREATE TABLE states (
+    command1 = "CREATE TABLE states (
             state_name VARCHAR(255) NOT NULL,
-            state_id SERIAL PRIMARY KEY
-        )
-        ,
-        CREATE TABLE cities (
+            state_id SERIAL PRIMARY KEY 
+            ) "
+        
+    command2 = "CREATE TABLE cities (
                 city_name VARCHAR(255) NOT NULL,
                 state_name VARCHAR(255) NOT NULL,
                 city_pop int(255) NOT NULL,
                 city_lat real(255) NOT NULL,
                 city_lon real(255) NOT NULL
-                )
-        ,
-      )
+                )"
+
     conn = None
     try:
         # read the connection parameters
@@ -29,8 +26,8 @@ def create_tables():
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
         # create table one by one
-        for command in commands:
-            cur.execute(command)
+        cur.execute(command1)
+        cur.execute(command2)
         # close communication with the PostgreSQL database server
         cur.close()
         # commit the changes
