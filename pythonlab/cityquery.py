571e08f315fbb299dfdc8021a7da9788b170f9e6
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-01-28 11:07:40 -0600

    updated query

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -28,4 +28,34 @@ def get_city():
         if conn is not None:
             conn.close()
 
-get_city()
+get_city() #northfield is not in the database
+
+def get_city_with_highest_pop():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        
+        conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="leen2",
+        user="leen2",
+        password="chip574pencil")
+        
+        cur = conn.cursor()
+        cur.execute("SELECT city_name ORDER BY city_pop DESC LIMIT 1")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+get_city_with_highest_pop()
