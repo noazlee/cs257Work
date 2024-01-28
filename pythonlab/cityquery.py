bed7838c0053f4ef7d2c1c5bf3e5c32b5000b4e4
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-01-28 11:00:15 -0600

    updated query

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+def get_city():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT city_name FROM cities WHERE city_name==Northfield")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+get_city()
