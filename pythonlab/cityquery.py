7dccd9d18d03a689b1f5b78f76285ec92f30f108
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-01-28 15:25:10 -0600

    updated query

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -213,8 +213,52 @@ def get_city_in_W():
         if conn is not None:
             conn.close()
 
+
+
 get_city_in_N() #anchorage
 get_city_in_S() #honolulu
 get_city_in_E() #honolulu
 get_city_in_W() #portland, maine
 
+state = input("Enter a US state ")
+
+def get_city_in_USER(state):
+
+    """ query data from the vendors table """
+    conn = None
+    try:
+        
+        conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="leen2",
+        user="leen2",
+        password="chip574pencil")
+        
+        cur = conn.cursor()
+
+        if(len(state)!=2)
+            cur.execute("""SELECT city_name, state_name
+            FROM cities
+            WHERE state_name LIKE """,state)
+            print("The number of parts: ", cur.rowcount)
+            row = cur.fetchone()
+        else:
+            cur.execute("""SELECT state_name
+            FROM states
+            WHERE state_id LIKE """,state)
+            print("The number of parts: ", cur.rowcount)
+            row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+get_city_in_USER(state)
