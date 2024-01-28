d444b047d020ddb4345da9b86d4c4d18f88c6c0a
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-01-28 13:34:54 -0600

    updated query

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -28,7 +28,7 @@ def get_city():
         if conn is not None:
             conn.close()
 
-#get_city() #northfield is not in the database
+get_city() #northfield is not in the database
 
 def get_city_with_highest_pop():
     """ query data from the vendors table """
@@ -43,7 +43,7 @@ def get_city_with_highest_pop():
         password="chip574pencil")
         
         cur = conn.cursor()
-        cur.execute("SELECT * FROM cities ORDER BY city_pop DESC LIMIT 1")
+        cur.execute("SELECT city_name FROM cities ORDER BY city_pop DESC LIMIT 1")
         print("The number of parts: ", cur.rowcount)
         row = cur.fetchone()
 
@@ -58,4 +58,34 @@ def get_city_with_highest_pop():
         if conn is not None:
             conn.close()
 
-get_city_with_highest_pop()
+get_city_with_highest_pop() # It is new york
+
+def get_city_in_MN():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        
+        conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="leen2",
+        user="leen2",
+        password="chip574pencil")
+        
+        cur = conn.cursor()
+        cur.execute("SELECT city_name FROM cities WHERE state_name==Minnesota ORDER BY city_pop LIMIT 1")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+get_city_in_MN()
