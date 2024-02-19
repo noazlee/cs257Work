dd8d13a5c3398f29bad01c8abd17a58179060bd4
Noah Lee <noahlee@Noahs-MacBook-Air.local>
2024-02-19 09:32:07 -0600

    edited python

--- csslab/js-example.py
+++ csslab/js-example.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     message = "Welcome to My Example Webpage."
-    message = message + " This text was produced by concatenating strings in Python!"
+    # message = message + " This text was produced by concatenating strings in Python!"
     return render_template("homepage.html", someText = message)
 
 if __name__ == '__main__':
